bd54abaad498b61fc8b898c3c5c85d4fd74f2dea
dcrlsn <c4r150n@gmail.com>
2021-12-12 15:45:09 -0500

    questions and generate markdown mostly done

--- index.js
+++ index.js
@@ -1,13 +1,98 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled  Table of Contents 
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "Please enter your github username."
+  },
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the project name?"
+  },
+  {
+    type: "input",
+    name: "repository",
+    message: "Please enter the repository name."
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a brief description of the project."
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please enter any installation command."
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please enter any test command.",
+  },
+  {
+    type: "checkbox",
+    name: "licenses",
+    message: "Please check any applicable licenses.",
+    choices: [
+      "Apache",
+      "BSD",
+      "GNU",
+      "MIT",
+      "Mozilla"
+    ]
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the project used for?"
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Add a contributor?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(function (answers) {
+      console.log(answers.licenses)
+      console.log(generateMarkdown(answers))
+    });
+}
 
 // Function call to initialize app
-init();
+init()
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache':
+      badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      break;
+    case 'BSD':
+      badge = `[![License](https://img.shields.io/badge/License-BSD-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+      break;
+    case 'GPL':
+      badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPL-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+      break;
+    case 'MIT':
+      badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      break;
+    case 'Mozilla':
+      badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+      break;
+  } return badge;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function generateTable(data) {
+  let tableOfContents = `\n## Table of Contents\n`;
+  if (data.installation) {
+    tableOfContents += `\n*[Installation](#installation)`
+  };
+  if (data.description) {
+    tableOfContents += `\n*[Description](#description)`
+  };
+  if (data.usage) {
+    tableOfContents += `\n*[Usage](#usage)`
+  };
+  if (data.contributors) {
+    tableOfContents += `\n*[Contributors](#contributors)`
+  };
+  if (data.licenses) {
+    tableOfContents += `\n*[Licenses](#licenses)`
+  };
+  tableOfContents += `\n[Questions](#questions)`
+  return tableOfContents;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  let readme = ``;
+  readme += `#${data.projectTitle}\n`;
+  readme += `${generateTable(data)}\n`;
+  if (data.installation) readme += `##Installation\n\`\`\`${data.installation}\`\`\`\n`
+  readme += `\n##Description\n${data.description}\n`;
+  if (data.usage) readme += `##Usage\n${data.usage}\n`
+  if (data.tests) readme += `##Tests\n\`\`\`${data.tests}\`\`\`\n`
+  if (data.contributors) readme += `##Contributors\n${data.contributors}\n`
+  if (data.licenses) {
+    readme += `##Licenses\n`
+    data.licenses.forEach(element => readme += `${renderLicenseBadge(element)}\n`)
+  }
+  readme += `##Questions\nFind me on Github at https://github.com/${data.username}/\nAny Questions please contact me at ${data.email}`
+  return readme;
 }
-
 module.exports = generateMarkdown;
